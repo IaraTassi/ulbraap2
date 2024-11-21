42a9305d200f005eb7be1e1cafababfaaf9682b0
IaraTassi <iaratassi@hotmail.com>
2024-11-21 09:37:17 -0300

    adiciona classe Moto com atributo cilindrada e altera a main para executar o programa

--- src/main/java/ulbraap2/com/Main.java
+++ src/main/java/ulbraap2/com/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Carro carro = new Carro("Honda", "Civic", 2019, 4);
         System.out.println(carro.informacoesVeiculo());
+
+        Moto moto = new Moto("Honda", "CG 160 Titan", 2018, 160);
+        System.out.println(moto.informacoesVeiculo());
+
     }
 }
